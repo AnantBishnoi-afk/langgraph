e1f834a76ba46f5f12f9b7893b6c391d4b7653d4
Anant <s41530075@gmail.com>
2025-12-23 15:41:23 +0530

    chat bot backend updated

--- bot_with_ui/backend.py
+++ bot_with_ui/backend.py
@@ -33,3 +33,7 @@ graph.add_edge(START,"chat_node")
 graph.add_edge("chat_node",END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+# chatbot.invoke({'messages':[HumanMessage(content='hi my name is chubul pandey')]},config={'configurable':{'thread_id':'thread-1'}})
+
+# print(chatbot.get_state(config={'configurable':{'thread_id' :'thread-1'}}).values['messages'])
