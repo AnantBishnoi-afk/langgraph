f80be1331cdb560f9d27deb24c74cbdffd07aa82
Anant <s41530075@gmail.com>
2025-12-23 15:40:45 +0530

    database addition to backend

---
+++ bot_with_ui/backend_with_db.py
@@ -0,0 +1,59 @@
+from langgraph.graph import StateGraph,START,END
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from typing import TypedDict, Annotated
+from langgraph.checkpoint.sqlite import SqliteSaver
+import os
+import operator
+from langgraph.graph.message import add_messages
+from langchain_core.messages import BaseMessage, HumanMessage
+import sqlite3
+
+
+
+
+
+
+
+load_dotenv()
+
+
+groq_api_key=os.getenv("GROQ_API_KEY")
+llm = ChatGroq(groq_api_key=groq_api_key,model_name="llama-3.1-8b-instant")
+
+class bot_State(TypedDict):
+
+    messages : Annotated[list[BaseMessage],add_messages]
+
+def chat_node(state : bot_State):
+    messages = state['messages']
+
+    reponse = llm.invoke(messages)
+
+    return {"messages":[reponse]}
+
+graph = StateGraph(bot_State)
+
+conn = sqlite3.connect(database='database.db',check_same_thread=False)
+
+checkpointer = SqliteSaver(conn=conn)
+
+graph.add_node("chat_node",chat_node)
+graph.add_edge(START,"chat_node")
+graph.add_edge("chat_node",END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+# response = chatbot.invoke({'messages':[HumanMessage(content='what is my name')]},config={'configurable':{'thread_id':'thread-1'}})
+
+# print(response)
+# print(chatbot.get_state(config={'configurable':{'thread_id' :'thread-1'}}).values['messages'])
+
+def retrieve_all_threads():
+    all_threads = set()
+    for chk in checkpointer.list(None):
+        all_threads.add(chk.config['configurable']['thread_id'])
+
+    return list(all_threads)
+
+
